172e1bcaabe6362f96847b8232fa9f42963b440e
Flenarn <smallmustard@gmail.com>
2025-03-16 20:44:16 +0100

    feat: A few server protocols.

--- src/engine/src/io/server/protocol/server_protocol.rs
+++ src/engine/src/io/server/protocol/server_protocol.rs
@@ -8,6 +8,24 @@ impl ServerProtocol {
     pub const fn new(id: i32, length: i32) -> ServerProtocol {
         ServerProtocol { id, length }
     }
-    
+
+    // Map
     pub const REBUILD_NORMAL: ServerProtocol = ServerProtocol::new(162, -2);
+
+    // Update
+    pub const NPC_INFO: ServerProtocol = ServerProtocol::new(32, -2);
+    pub const PLAYER_INFO: ServerProtocol = ServerProtocol::new(225, -2);
+
+    // var{p, c, bit}
+    pub const CLIENT_SETVARC_SMALL: ServerProtocol = ServerProtocol::new(65, 5);
+    pub const CLIENT_SETVARC_LARGE: ServerProtocol = ServerProtocol::new(69, 8);
+    
+    // Misc.
+    pub const LOGOUT: ServerProtocol = ServerProtocol::new(86, 0);
+    pub const UPDATE_RUNENERGY: ServerProtocol = ServerProtocol::new(234, 1);
+    pub const UPDATE_RUNWEIGHT: ServerProtocol = ServerProtocol::new(159, 2);
+    pub const UPDATE_REBOOT_TIME: ServerProtocol = ServerProtocol::new(85, 2);
+    pub const MIDI_SONG: ServerProtocol = ServerProtocol::new(4, 2);
+    pub const MIDI_JINGLE: ServerProtocol = ServerProtocol::new(208, 5);
+    pub const SYNTH_SOUND: ServerProtocol = ServerProtocol::new(172, 5);
 }
