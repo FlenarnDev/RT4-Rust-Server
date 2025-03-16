20307528d59463d7c8b63ac3357f673625257bcf
Flenarn <smallmustard@gmail.com>
2025-03-16 09:36:27 +0100

    feat: Expand and update XTEA handling.

--- src/cache/src/xtea.rs
+++ src/cache/src/xtea.rs
@@ -1,24 +1,72 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct XTEAKey {
-    pub k0: i32,
-    pub k1: i32,
-    pub k2: i32,
-    pub k3: i32,
-}
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::Read;
+use std::sync::OnceLock;
+use std::time::Instant;
+use std::error;
+use log::{error, info};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Deserialize, Serialize)]
+pub struct XTEAKey(pub i32, pub i32, pub i32, pub i32);
 
 impl XTEAKey {
-    pub fn new(k0: i32, k1: i32, k2: i32, k3: i32) -> Self {
-        XTEAKey { k0, k1, k2, k3 }
-    }
-    
-    pub const ZERO: Self = XTEAKey { k0: 0, k1: 0, k2: 0, k3: 0 };
-    
+    pub const ZERO: Self = XTEAKey(0, 0, 0, 0);
+
+    /// Check if XTEA key is equal to: {0, 0, 0, 0}
     pub fn is_zero(&self) -> bool {
-        self.k0 == 0 && self.k1 == 0 && self.k2 == 0 && self.k3 == 0
+        self.0 == 0 && self.1 == 0 && self.2 == 0 && self.3 == 0
     }
-    
+
     /// Convert to array
     pub fn to_array(&self) -> [i32; 4] {
-        [self.k0, self.k1, self.k2, self.k3]
+        [self.0, self.1, self.2, self.3]
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct XTEAData {
+    archive: i32,
+    group: i32,
+    #[serde(rename= "name_hash")]
+    name_hash: i32,
+    name: String,
+    mapsquare: i32,
+    key: XTEAKey,
+}
+
+static XTEA_MAP: OnceLock<HashMap<i32, XTEAKey>> = OnceLock::new();
+pub fn initialize_xtea() -> Result<bool, Box<dyn error::Error>> {
+    let start = Instant::now();
+    info!("Initializing XTEA module.");
+    
+    let mut file = File::open("../../src/cacheLocal/XTEAkeys.json")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    
+    let keys_list: Vec<XTEAData> = serde_json::from_str(&contents)?;
+    
+    let mut map = HashMap::new();
+    
+    for data in keys_list {
+        let xtea_key = XTEAKey(data.key.0, data.key.1, data.key.2, data.key.3);
+        map.insert(data.mapsquare, xtea_key);
+    }
+
+    if let Err(_) = XTEA_MAP.set(map) {
+        error!("XTEA map was already initialized");
+    }
+
+    info!("XTEA count: {}", XTEA_MAP.get().unwrap().len());
+    info!("XTEA Module initiated in: {}ms.", start.elapsed().as_millis());
+
+    Ok(true)
+}
+
+pub fn get_xtea_key_by_mapsquare(mapsquare: i32) -> XTEAKey {
+    if let Some(map) = XTEA_MAP.get() {
+        map.get(&mapsquare).copied().unwrap_or(XTEAKey::ZERO)
+    } else {
+        XTEAKey::ZERO
     }
 }
