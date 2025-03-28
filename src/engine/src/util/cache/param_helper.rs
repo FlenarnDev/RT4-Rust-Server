c243fb262ec304adb342a04db9dda91c987a1336
flenarn <smallmustard@gmail.com>
2025-03-28 14:26:12 +0100

    feat: [param_helper.rs] established, helper class for handling [Params] on config types.

---
+++ src/engine/src/util/cache/param_helper.rs
@@ -0,0 +1,31 @@
+use std::collections::HashMap;
+use crate::io::packet::Packet;
+
+#[derive(Debug)]
+pub enum ParamValue {
+    String(String),
+    Integer(i32),
+}
+
+pub type Params = HashMap<i32, ParamValue>;
+
+pub fn decode_params(packet: &mut Packet) -> Params {
+    let count = packet.g1();
+    let mut params: Params = HashMap::with_capacity(count as usize);
+    
+    
+    for _ in 0..count {
+        let is_string = packet.g1() == 1;
+        let key = packet.g3();
+        
+        let value = if is_string {
+            ParamValue::String(packet.gjstr(0))
+        } else {
+            ParamValue::Integer(packet.g4())
+        };
+        
+        params.insert(key, value);
+    }
+    
+    params
+}
