240bc256efc829912d0cceb966bd26bc943a2b56
flenarn <smallmustard@gmail.com>
2025-03-28 13:23:15 +0100

    chore: Cleanup player ops.

--- src/engine/src/script/handlers/player_ops.rs
+++ src/engine/src/script/handlers/player_ops.rs
@@ -6,13 +6,6 @@ use std::sync::OnceLock;
 use crate::engine::Engine;
 use crate::io::server::model::message_game::Message_Game;
 
-#[inline(always)]
-fn handle_mes(state: &mut ScriptState) {
-    let message = state.pop_string();
-    let player = Engine::get().players.get_mut(state.get_active_player().unwrap().get_pid()).unwrap();
-    player.write(Message_Game::new(message));
-}
-
 pub fn get_player_ops() -> &'static CommandHandlers {
     static HANDLERS: OnceLock<CommandHandlers> = OnceLock::new();
     
@@ -21,10 +14,12 @@ pub fn get_player_ops() -> &'static CommandHandlers {
 
         handlers.insert(
             ScriptOpcode::MES as i32,
-            handle_mes
+            |state: &mut ScriptState| {
+                let player = Engine::get().players.get_mut(state.get_active_player().unwrap().get_pid()).unwrap();
+                player.write(Message_Game::new(state.pop_string()));
+            }
         );
 
         handlers  
     })
-    
 }
